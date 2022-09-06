49fdf6a0eff7fab32d49258f910a7cba44bdd933
Dan Brough <dan@danbrough.org>
2022-09-06 17:17:05 +1200

    update

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+
+plugins {
+  kotlin("multiplatform") version "1.7.10" apply false
+}
+
+
+
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,11 +8,5 @@ pluginManagement {
 }
 
 
-
-plugins {
-  id("de.fayard.refreshVersions") version "0.40.2"
-}
-
-
 rootProject.name = "kotlinxtras"
 
