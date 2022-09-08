17d8e077110a8dd1612fe9ae743fd0a954b277ea
Dan Brough <dan@mac.local>
2022-09-08 12:56:43 +1200

    update

--- openssl/build.sh
+++ openssl/build.sh
@@ -2,8 +2,8 @@
 
 cd "$(dirname "$0")"
 
-TARGET=android-x86
-export PREFIX="/opt/kotlinxtras/libs/openssl/OpenSSL_1_1_1q/androidNativeX86"
+TARGET=android-x86_64
+export PREFIX="/opt/kotlinxtras/libs/openssl/OpenSSL_1_1_1q/androidNativeX64"
 
 API=21
 export ANDROID_API=$API
