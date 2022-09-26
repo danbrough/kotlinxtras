e5adf5751f778179920b4688607c991ad73fe0e9
Dan Brough <dan@danbrough.org>
2022-09-26 14:04:21 +1300

    update

--- plugin/src/main/kotlin/org/danbrough/kotlinxtras/binaries/BinariesPlugin.kt
+++ plugin/src/main/kotlin/org/danbrough/kotlinxtras/binaries/BinariesPlugin.kt
@@ -18,6 +18,9 @@ import org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeTest
 import org.jetbrains.kotlin.gradle.tasks.KotlinNativeCompile
 import org.jetbrains.kotlin.konan.target.KonanTarget
 
+const val DEFAULT_CURL_VERSION = "curl-7_85_0_a"
+const val DEFAULT_OPENSSL_VERSION = "OpenSSL_1_1_1q_a"
+
 data class BinDep(val group: String?, val name: String, val version: String?)
 
 val defaultSupportedTargets = mutableSetOf(
@@ -35,13 +38,13 @@ val defaultSupportedTargets = mutableSetOf(
 open class BinariesExtension {
   internal var binDeps: MutableSet<BinDep> = mutableSetOf()
 
-  var message:String = "Hello World"
+  var message: String = "Hello World"
 
-  fun enableCurl(version: String = "curl-7_85_0_a") {
+  fun enableCurl(version: String = DEFAULT_CURL_VERSION) {
     binDeps.add(BinDep("org.danbrough.kotlinxtras", "curl", version))
   }
 
-  fun enableOpenSSL(version: String = "OpenSSL_1_1_1q_a") {
+  fun enableOpenSSL(version: String = DEFAULT_OPENSSL_VERSION) {
     binDeps.add(BinDep("org.danbrough.kotlinxtras", "openssl", version))
   }
 
@@ -103,10 +106,74 @@ fun Project.configurePrecompiledBinaries() {
   }
 }
 
+private fun Project.configureTask( task: Task) {
+  if (task is KotlinNativeCompile) {
+    println("TASK: $task target: ${task.target}")
+  }
+}
+
+fun Project.configureBinaries(project: Project) {
+
+  project.tasks.forEach { project.configureTask( it) }
+  project.childProjects.values.forEach {
+    it.configureBinaries(it)
+  }
+
+}
+
 class BinariesPlugin : Plugin<Project> {
+
+
   override fun apply(project: Project) {
     val binaries = project.extensions.create("binaries", BinariesExtension::class.java)
-    val kotlinMPP = project.extensions.findByType<KotlinMultiplatformExtension>()
-    println("PROJECT $project MPP: $kotlinMPP")
+
+    println("DOING BINARIES STUFF")
+    val preCompiled: Configuration by project.configurations.creating {
+      isTransitive = false
+    }
+
+
+    val konanTargets = project.extensions.getByType<KotlinMultiplatformExtension>().targets.withType<KotlinNativeTarget>()
+      .map { it.konanTarget }.distinct()
+
+    project.dependencies {
+      binaries.binDeps.forEach { binDep ->
+        konanTargets.forEach { target ->
+          val binDepLib =
+            "${binDep.group}:${binDep.name}${target.platformName.capitalized()}Binaries:${binDep.version}"
+          project.logger.log(LogLevel.INFO, "Adding binary support with $binDepLib")
+          preCompiled(binDepLib)
+        }
+      }
+    }
+
+    preCompiled.resolvedConfiguration.resolvedArtifacts.forEach { artifact ->
+      project.tasks.register<Copy>("extract${artifact.name.capitalized()}") {
+        group = xtrasTaskGroup
+
+        from(project.zipTree(artifact.file).matching {
+          exclude("**/META-INF")
+          exclude("**/META-INF/*")
+        })
+        into(project.rootProject.buildDir.resolve("kotlinxtras"))
+      }
+    }
+
+    binaries.binDeps.forEach { binDep ->
+      project.tasks.withType(KotlinNativeCompile::class).forEach {
+        project.logger.log(LogLevel.INFO,"adding extract dependency on ${binDep.name} for ${it.name}")
+        val konanTarget = KonanTarget.predefinedTargets[it.target]!!
+        it.dependsOn("extract${binDep.name.capitalized()}${konanTarget.platformName.capitalized()}Binaries")
+      }
+    }
+    /*project.extensions.getByType<KotlinMultiplatformExtension>().apply {
+      println("PROJECT $project MPP: $this")
+
+    }
+    */
+
+
+
+
   }
 }
