1ca306054508acf79bd773377f30832ef4a5a2df
Dan Brough <dan@danbrough.org>
2022-09-27 12:52:19 +1300

    added stately

--- publish.sh
+++ publish.sh
@@ -9,7 +9,7 @@ if [ ! -z "$1" ]; then
 fi
 
 if [ "$(uname)" = "Darwin" ]; then
-  ./gradlew -PsignPublications -PpublishDocs`cat mac_targets.txt`
+  ./gradlew -PsignPublications -PpublishDocs `cat mac_targets.txt`
   exit 0
 fi
 
