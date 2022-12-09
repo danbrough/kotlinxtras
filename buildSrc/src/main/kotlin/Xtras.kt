c559eacf42a2b0bcbf68b5cb89023c8c528d6bd7
Dan Brough <dan@danbrough.org>
2022-12-09 13:58:27 +1300

    update

--- buildSrc/src/main/kotlin/Xtras.kt
+++ buildSrc/src/main/kotlin/Xtras.kt
@@ -7,7 +7,7 @@ import org.gradle.plugin.use.PluginDependencySpec
 
 object Xtras {
   const val projectGroup = "org.danbrough.kotlinxtras"
-  const val version = "0.0.3-beta02"
+  const val version = "0.0.3-beta01"
   const val publishingVersion = "0.0.3-beta01"
   const val repoName = "xtras"
 }
