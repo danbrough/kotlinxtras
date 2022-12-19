677d873c804504e7df2ec156a6b8883aa509b554
Dan Brough <dan@danbrough.org>
2022-12-19 13:56:03 +1300

    update

--- core/src/main/kotlin/org/danbrough/kotlinxtras/CorePlugin.kt
+++ core/src/main/kotlin/org/danbrough/kotlinxtras/CorePlugin.kt
@@ -3,7 +3,7 @@ package org.danbrough.kotlinxtras
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-const val CORE_PUBLISHING_PACKAGE = "org.danbrough.kotlinxtras.core"
+const val CORE_PUBLISHING_PACKAGE = "org.danbrough.kotlinxtras.xtras"
 class CorePlugin: Plugin<Project> {
   override fun apply(target: Project) {
   }
