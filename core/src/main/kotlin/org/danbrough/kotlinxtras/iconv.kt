fe92f022b86b8af32dd22e78993fe7512e720c30
Dan Brough <dan@danbrough.org>
2023-01-17 20:21:05 +1300

    update

--- core/src/main/kotlin/org/danbrough/kotlinxtras/iconv.kt
+++ core/src/main/kotlin/org/danbrough/kotlinxtras/iconv.kt
@@ -7,44 +7,49 @@ import org.danbrough.kotlinxtras.binaries.*
 import org.gradle.api.Project
 
 const val XTRAS_ICONV_EXTN_NAME = "iconv"
+const val XTRAS_ICONV_VERSION_NAME = "1.17"
+const val XTRAS_ICONV_SOURCE_URL = "https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.17.tar.gz"
 
+fun Project.enableIconv(
+  extnName: String = XTRAS_ICONV_EXTN_NAME,
+  versionName: String = XTRAS_ICONV_VERSION_NAME,
+  sourceURL: String = XTRAS_ICONV_SOURCE_URL,
+  config: LibraryExtension.() -> Unit = {}
+) =
+  extensions.findByName(extnName) as? LibraryExtension
+    ?: project.registerLibraryExtension(extnName) {
+      publishingGroup = CORE_PUBLISHING_PACKAGE
+      version = versionName
 
-fun Project.enableIconv(extnName: String = XTRAS_ICONV_EXTN_NAME, config:LibraryExtension.()->Unit = {}) =
-  extensions.findByName(extnName) as? LibraryExtension ?:
+      download(sourceURL) {
+        stripTopDir = true
+      }
 
-  project.registerLibraryExtension(extnName) {
-    publishingGroup = CORE_PUBLISHING_PACKAGE
-    version = "1.17"
+      configure { target ->
+        commandLine(
+          "./configure",
+          "-C",
+          "--enable-static",
+          "--host=${target.hostTriplet}",
+          "--prefix=${buildDir(target)}"
+        )
+        outputs.file(workingDir.resolve("Makefile"))
+      }
 
-    download("https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.17.tar.gz") {
-      stripTopDir = true
-    }
+      build {
+        commandLine(binaries.makeBinary, "install")
+      }
 
-    configure { target ->
-      commandLine(
-        "./configure",
-        "-C",
-        "--enable-static",
-        "--host=${target.hostTriplet}",
-        "--prefix=${buildDir(target)}"
-      )
-      outputs.file(workingDir.resolve("Makefile"))
-    }
-
-    build {
-      commandLine(binaries.makeBinary, "install")
-    }
-
-    cinterops {
-      headers = """
+      cinterops {
+        headers = """
           |headers = iconv.h libcharset.h
           |linkerOpts = -liconv
           |package = libiconv
           |""".trimMargin()
-    }
+      }
 
-    config()
-  }
+      config()
+    }
 
 
 
