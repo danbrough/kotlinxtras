6577d21db65b234450f4e1394a081e86beca3c2b
Dan Brough <dan@danbrough.org>
2023-08-15 10:50:40 +1200

    update

--- plugin/src/main/kotlin/org/danbrough/kotlinxtras/supportedTargets.kt
+++ plugin/src/main/kotlin/org/danbrough/kotlinxtras/supportedTargets.kt
@@ -7,11 +7,24 @@ import org.jetbrains.kotlin.konan.target.KonanTarget
 
 /**
  * Declares the kotlin native targets that are currently supported
+ *
+ * The following targets are marked as deprecated in 1.8.20 and will be removed in 1.9.20:
+ *
+ *     iosArm32 – 32-bit iOS Apps were deprecated a long time ago, and the last iOS release that supported them was iOS 10, which was released back in 2016. Furthermore, as of version 14 the most recent release, Xcode no longer builds 32-bit binaries.
+ *     watchosX86 – This is an obsolete simulator for Intel Macs. Use the watchosX64 target instead.
+ *     linuxArm32Hfp – When it comes to 32-bit ARM, it is hard to provide a single universal Kotlin/Native target because of the variety of options: ARMv5, ARMv6, ARMv7; hfp, sfp; multiple toolchains; etc. To some extent, this is true for other Linux targets, but linuxArm32Hfp has proven to be especially hard to support.
+ *     linuxMips32 and linuxMipsel32 – We haven’t seen any real demand for these two.
+ *     mingwX86 – Support for this target requires a lot of resources while user demand remains low. Furthermore, mingwX64 is better suited for the majority of known use cases anyway.
+ *     wasm32 – We’re deprecating this one in favor of the new fully-fledged Kotlin/Wasm toolchain. Check it out!
+ *
  */
 fun KotlinMultiplatformExtension.declareSupportedTargets() {
   if (HostManager.hostIsMac) {
     macosX64()
     macosArm64()
+    iosArm64()
+    iosX64()
+    watchosX64()
   } else {
     androidNativeX86()
     androidNativeX64()
