6873263f641580e072bfb769ead951871c77e54c
Dan Brough <dan@danbrough.org>
2023-08-30 09:00:03 +1200

    update

--- plugin/src/main/kotlin/org/danbrough/kotlinxtras/supportedTargets.kt
+++ plugin/src/main/kotlin/org/danbrough/kotlinxtras/supportedTargets.kt
@@ -24,40 +24,43 @@ fun KotlinMultiplatformExtension.declareSupportedTargets() {
     macosArm64()
     iosArm64()
     iosX64()
+    tvosArm64()
+    tvosX64()
+    watchosArm64()
+    watchosX64()
     // watchosX64()
-  } else {
-    androidNativeX86()
-    androidNativeX64()
-    androidNativeArm32()
-    androidNativeArm64()
-
-    mingwX64()
-
-    linuxX64()
-    linuxArm64()
-    linuxArm32Hfp()
   }
+  androidNativeX86()
+  androidNativeX64()
+  androidNativeArm32()
+  androidNativeArm64()
 
+  mingwX64()
 
-  /*
-  //TODO
-  iosArm32()
-  iosArm64()
-  iosSimulatorArm64()
-  iosX64()
-
-  tvosArm64()
-  tvosX64()
-  tvosSimulatorArm64()
-
-  watchosArm32()
-  watchosArm64()
-  watchosX64()
-  watchosX86()
-  watchosSimulatorArm64()
-   */
-
+  linuxX64()
+  linuxArm64()
+  linuxArm32Hfp()
 }
+
+
+/*
+//TODO
+iosArm32()
+iosArm64()
+iosSimulatorArm64()
+iosX64()
+
+tvosArm64()
+tvosX64()
+tvosSimulatorArm64()
+
+watchosArm32()
+watchosArm64()
+watchosX64()
+watchosX86()
+watchosSimulatorArm64()
+ */
+
 
 val xtrasSupportedTargets: List<KonanTarget> = listOf(
   KonanTarget.LINUX_X64,
