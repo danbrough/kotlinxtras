caea159d6848b68be36a273750492b69599482bf
Dan Brough <dan@danbrough.org>
2023-09-21 19:01:23 +1200

    update

--- test2/src/jvmMain/kotlin/demo/jvm.kt
+++
@@ -1,6 +0,0 @@
-package demo
-
-actual fun demo(args:Array<String>){
-  println("RUNNING THE DEMO")
-  log.trace("running the jvm demo")
-}
--- test2/src/posixMain/kotlin/demo/demo.kt
+++
@@ -1,7 +0,0 @@
-package demo
-
-actual fun demo(args: Array<String>){
-  println("RUNNING NATIVE DEMO")
-  log.info("running the native demo")
-  curlDemo(args)
-}
