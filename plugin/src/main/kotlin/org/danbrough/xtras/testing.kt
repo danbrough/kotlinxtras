09c74b4b4b607966f564a3c226cf24f4c5370801
Dan Brough <dan@danbrough.org>
2023-10-12 08:04:49 +1300

    update

---
+++ plugin/src/main/kotlin/org/danbrough/xtras/testing.kt
@@ -0,0 +1,25 @@
+package org.danbrough.xtras
+
+import org.gradle.api.Project
+import org.gradle.api.tasks.testing.AbstractTestTask
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.gradle.kotlin.dsl.withType
+
+
+fun Project.enableVerboseTesting() {
+  tasks.withType<AbstractTestTask> {
+    testLogging {
+      events = setOf(
+        TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED
+      )
+      exceptionFormat = TestExceptionFormat.FULL
+      showStandardStreams = true
+      showStackTraces = true
+    }
+
+    outputs.upToDateWhen {
+      false
+    }
+  }
+}
